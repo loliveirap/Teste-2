97a7cab81f384647ca72b7490d7ed769e664d538
loliveirap <lep_o@hotmail.com>
2017-08-18 00:21:10 -0300

    Add files via upload

---
+++ Controllers/AutenticacaoController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TesteWebApiContatos.Controllers
+{
+    public class AutenticacaoController : Controller
+    {
+        // GET: Autenticacao
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string senha)
+        {
+            var api = new HttpClient();
+            api.BaseAddress = new Uri("http://localhost:7831/api/");
+
+            var content = new FormUrlEncodedContent(new[] {
+                new KeyValuePair<string, string>("email", email),
+                new KeyValuePair<string, string>("senha", senha)
+            });
+
+            var retorno = api.PostAsync("Autenticacao", content).Result;
+            if (retorno.IsSuccessStatusCode) {
+                //var _retorno = retorno.Content.ReadAsStringAsync().Result;
+		
+		FormsAuthentication.SetAuthCookie(email, false);
+                return RedirectToAction("Index", "Contato");
+            }
+            return View();
+        }
+	
+	public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Autenticacao");
+        }
+    }
+}
+
+
+
+            //if(txtUsuario == "convenios" || txtUsuario == "CONVENIOS" || txtUsuario == "Convenios" && txtSenha == "admin2017")
+            //{
+            //    FormsAuthentication.SetAuthCookie(txtUsuario, false);
+            //    return RedirectToAction("Index", "Home");
+            //}
+            //else
+            //{
+            //    TempData.Add("Erro", "Usuário ou senha incorretos.");
+            //    return View();
+            //}
---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using TesteWebApiContatos.Models;
+
+namespace TesteWebApiContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        // GET: Contato
+        public ActionResult Index()
+        {
+            var api = new HttpClient();
+            api.BaseAddress = new Uri("http://localhost:7831/api/");
+
+            var retorno = api.GetAsync("Contato").Result;
+            
+            if (retorno.IsSuccessStatusCode)
+            {
+                var _retorno = retorno.Content.ReadAsAsync<IEnumerable<Contato>>().Result;                
+
+                var _contato = new List<Contato>();
+
+                foreach(var item in _retorno)
+                {
+                    _contato.Add(new Contato
+                    {
+                        Nome = item.Nome,
+                        Email = item.Email
+                    });
+                }
+
+                return View(_contato);
+            }
+            return View();
+        }
+    }
+}
