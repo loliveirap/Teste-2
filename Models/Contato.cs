14e13403e6e78c2372a8741ba0863cab72ce3469
loliveirap <lep_o@hotmail.com>
2017-08-18 00:22:01 -0300

    Add files via upload

---
+++ Models/Contato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TesteWebApiContatos.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool IsParente { get; set; }
+        public string Empresa { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public DateTime Nascimento { get; set; }
+    }
+}
